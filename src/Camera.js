1a4f80c9774051cdfc78ee24fe74420c6276f7f0
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-11-24 16:25:24 +0530

    Add files via upload

---
+++ src/Camera.js
@@ -0,0 +1,10 @@
+import React, { useRef, useEffect } from 'react'
+import { useThree } from 'react-three-fiber'
+export const Camera = (props) => {
+  const ref = useRef()
+  const { setDefaultCamera } = useThree()
+  useEffect(() => {
+    setDefaultCamera(ref.current)
+  }, )
+  return <perspectiveCamera ref={ref} {...props}/>
+}
